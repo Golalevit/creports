97b92b94fc116e555f0d554eb6f2c50890462197
Dmitry <kromez12@gmail.com>
2020-09-01 07:51:52 +0300

    add users multiselect

--- src/pages/repository-page/types.ts
+++ src/pages/repository-page/types.ts
@@ -3,5 +3,5 @@ import { RepositoriesResponse } from '@store/repositories/types';
 export interface FiltersConfig {
   startDate: Date | null;
   endDate: Date | null;
-  repositories: RepositoriesResponse[];
+  list: any[];
 }
--- src/store/repositories/repositories.selectors.ts
+++ src/store/repositories/repositories.selectors.ts
@@ -7,3 +7,10 @@ export const getRepositoriesData = createSelector(
   getRepositoriesState,
   ({ repositoriesLoading, repositories }) => ({ repositoriesLoading, repositories }),
 );
+
+export const getUsersData = createSelector(getRepositoriesState, ({ users, usersLoading }) => ({
+  users: users.map((user, index) => ({ value: index, label: user })),
+  usersLoading,
+}));
+
+export const getUsers = createSelector(getRepositoriesState, (state) => state.users);
